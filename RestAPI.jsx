0a5f7c3269807e43342db4406f98f8f0d4241098
SodiumZar <nazarya2005@gmail.com>
2025-11-18 19:19:44 +0800

    feat: adding navigation bar feature

--- RestAPI.jsx
+++ RestAPI.jsx
@@ -1,2 +1,22 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Navbar from './src/components/Navbar/index.jsx';
+
+const RestAPI = () => {
+    return (
+        <>
+        <div className="min-h-screen bg-gray-100">
+            <Navbar />
+            
+            {/* Other components will go here */}
+            <main>
+                <div className="container mx-auto p-8">
+                    <h1 className="text-3xl font-bold">Welcome to My Portfolio</h1>
+                </div>
+            </main>
+        </div>
+        </>
+    );
+};
+
+export default RestAPI;
